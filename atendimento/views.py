d9093caa48dbd99e4c957c5826dea95dd1d1556a
Aluno <you@example.com>
2018-11-22 21:04:39 -0300

    cliente

--- atendimento/views.py
+++ atendimento/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Cliente
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
+
+def cliente_list(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'cliente/list.html', {'clientes':clientes})
