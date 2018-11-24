facb239b8b0e53273a99baade0a0954de9668e6d
Aluno <you@example.com>
2018-11-24 01:08:49 -0300

    cliente

--- atendimento/models.py
+++ atendimento/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=50),
-    dt_nasc = models.DateField(),
-    cpf = models.CharField(max_length=11),
+    nome = models.CharField(max_length=50)
+    dt_nasc = models.DateField()
+    cpf = models.CharField(max_length=11)
     sexo = models.CharField(max_length=1)
