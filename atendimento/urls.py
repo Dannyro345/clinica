8fb90bf93f3793b83fc7010c16468125eb8ab503
Aluno <you@example.com>
2019-02-01 01:47:12 -0300

    Criando o form no cliente e modificando a listagem.

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('cliente/', views.cliente_list),
     path('cliente/<int:cliente_id>/', views.cliente_show),
     path('medico/', views.medico_list),
-    path('medico/<int:medico_id>/', views.medico_show)
+    path('medico/<int:medico_id>/', views.medico_show),
+    path('cliente/form/', views.cliente_form)
    
 ]
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from . models import Cliente, Medico
+from .models import Cliente, Medico
+from .forms import ClienteForm
+
+
 
 # Create your views here.
 
@@ -15,6 +18,16 @@ def cliente_show(request, cliente_id):
     cliente = Cliente.objects.get(pk=cliente_id)
     return render(request, 'cliente/show.html', {'cliente':cliente})      
 
+def cliente_form(request):
+    if (request.method == 'POST'):
+        form = ClienteForm(request.POST)
+        form.save()
+        return redirect('/atendimento/cliente/')     
+
+    else:
+        form = ClienteForm()
+        return render(request, 'cliente/form.html', {'form':form})
+
 def medico_list(request):
     medicos = Medico.objects.all()
     return render(request, 'medico/list.html', {'medicos':medicos})
@@ -23,4 +36,3 @@ def medico_show(request, medico_id):
     medico = Medico.objects.get(pk=medico_id)
     return render(request, 'medico/show.html', {'medico':medico})      
 
-  
