382a087e7403f331e203e5d136a21cf38e53eb45
Aluno <you@example.com>
2019-02-01 20:25:07 -0300

    Modificamos os formularios usando o label fazendo com que eles peguem os dados automaticamente.

--- atendimento/forms.py
+++ atendimento/forms.py
@@ -1,12 +1,24 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Cliente, Medico
 
 class ClienteForm(ModelForm):
     class Meta:
         model = Cliente
         fields = '__all__'
+        widgets = {
+            'nome': TextInput(attrs={'class':'form-control'}),
+            'dt_nasc': TextInput(attrs={'class':'form-control'}),
+            'cpf': TextInput(attrs={'class':'form-control'}),
+            'sexo': TextInput(attrs={'class':'form-control'}),
+        }
 
 class MedicoForm(ModelForm):
     class Meta:
         model = Medico
         fields = '__all__'
+        widgets = {
+            'nome': TextInput(attrs={'class':'form-control'}),
+            'dt_nasc': TextInput(attrs={'class':'form-control'}),
+            'cpf': TextInput(attrs={'class':'form-control'}),
+            'sexo': TextInput(attrs={'class':'form-control'}),
+        }
